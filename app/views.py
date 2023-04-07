88703447e900206b9fe80f4bd2ead404daea43d9
victor_dev <victor.application.dev@gmail.com>
2023-04-07 18:12:34 +0100

    API_ROUTES

--- app/views.py
+++ app/views.py
@@ -15,3 +15,35 @@ def getStores(request):
     url = 'https://api.printful.com/stores'
     response = requests.get(url, headers=headers)
     return JsonResponse(response.json())
+
+def getProducts(request):
+    key = os.environ.get("STORE_TOKEN")
+    headers = {'Authorization': f'Bearer {key}'}
+    url = 'https://api.printful.com/store/products'
+    response = requests.get(url, headers=headers)
+    return JsonResponse(response.json())
+
+def getProducts_details(request, product_id):
+    key = os.environ.get("STORE_TOKEN")
+    headers = {'Authorization': f'Bearer {key}'}
+    url = f'https://api.printful.com/store/products/{product_id}'
+    response = requests.get(url, headers=headers)
+    return JsonResponse(response.json())
+def getProducts_tshirts(request):
+    key = os.environ.get("STORE_TOKEN")
+    headers = {'Authorization': f'Bearer {key}'}
+    url = 'https://api.printful.com/store/products?category_id=24'
+    response = requests.get(url, headers=headers)
+    return JsonResponse(response.json())
+def getProducts_hoodies(request):
+    key = os.environ.get("STORE_TOKEN")
+    headers = {'Authorization': f'Bearer {key}'}
+    url = 'https://api.printful.com/store/products?category_id=7'
+    response = requests.get(url, headers=headers)
+    return JsonResponse(response.json())
+def getProducts_hats(request):
+    key = os.environ.get("STORE_TOKEN")
+    headers = {'Authorization': f'Bearer {key}'}
+    url = 'https://api.printful.com/store/products?category_id=15'
+    response = requests.get(url, headers=headers)
+    return JsonResponse(response.json())
