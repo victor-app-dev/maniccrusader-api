7b512bf830ecaf0b85f3c4cdc6d3fe2aa5f76998
victor_dev <victor.application.dev@gmail.com>
2023-07-29 16:08:53 +0100

    a lot of shit

--- app/urls.py
+++ app/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path
-from .views import home_page, getStores, getProducts,getProducts_details,getProducts_tshirts,getProducts_hoodies,getProducts_hats, getVariant_details
+from .views import home_page, getStores, getProducts,getProducts_details,getProducts_tshirts,getProducts_hoodies,getProducts_hats, getVariant_details, getCountires, getShippingDetails
 
 urlpatterns = [
     path('', home_page),
-    path('api/stores/', getStores, name='get_Stores'),
-    path('api/products/', getProducts, name='get_Products'),
-    path('api/products-details/<int:product_id>/', getProducts_details, name='get_Products_details'),
-    path('api/products-tshirts/', getProducts_tshirts ),
-    path('api/products-hoodies/', getProducts_hoodies),
+    path('api/stores/', getStores, name='gets all stores associated with your account.'),
+    path('api/products/', getProducts, name='gets all products from your store.'),
+    path('api/products-details/<int:product_id>/', getProducts_details, name='gets product details, i.e. the variants etc.'),
+    path('api/products-tshirts/', getProducts_tshirts),
+    path('api/products-hoodies/', getProducts_hoodies ),
     path('api/products-hats/', getProducts_hats),
-    path('api/variant-details/<int:variant_id>/', getVariant_details)
+    path('api/variant-details/<int:variant_id>/', getVariant_details, name='gets variants details.'),
+    path('api/countries/', getCountires, name='gets all countries you can ship to.'),
+    path('api/shipping_rates/<str:payload>/', getShippingDetails, name='shipping_rates'),
+
 ]
--- app/views.py
+++ app/views.py
@@ -3,6 +3,7 @@ from rest_framework import viewsets
 import requests
 from django.http import JsonResponse
 import os
+import json
 #import serializers clases
 #import models classes
 # Create your views here.
@@ -53,3 +54,50 @@ def getVariant_details(request, variant_id):
     url = f'https://api.printful.com/store/variants/{variant_id}'
     response = requests.get(url, headers=headers)
     return JsonResponse(response.json())
+def getCountires(request):
+    key = os.environ.get("STORE_TOKEN")
+    headers = {'Authorization': f'Bearer {key}'}
+    url = f'https://api.printful.com/countries'
+    response = requests.get(url, headers=headers)
+    return JsonResponse(response.json())
+# def getShippingDetails(request, payload):
+#     key = os.environ.get("STORE_TOKEN")
+#     headers = {'Authorization': f'Bearer {key}'}
+#     url = 'https://api.printful.com/shipping/rates'
+#     payload_dict = json.loads(payload)
+#     response = requests.post(url, headers=headers, json=payload_dict)
+#     return JsonResponse(response.json())
+
+def getShippingDetails(request):
+    # Retrieve the API key securely, using environment variables or Django settings
+    key = os.environ.get("STORE_TOKEN")
+
+    if not key:
+        return JsonResponse({"error": "API key not available."}, status=500)
+
+    # Ensure that the view function only responds to POST requests
+    if request.method != "POST":
+        return JsonResponse({"error": "Invalid request method."}, status=405)
+
+    try:
+        # Get the payload from the request body
+        payload = json.loads(request.body)
+    except json.JSONDecodeError:
+        return JsonResponse({"error": "Invalid JSON payload."}, status=400)
+
+    url = 'https://api.printful.com/shipping/rates'
+    headers = {'Authorization': f'Bearer {key}'}
+
+    try:
+        response = requests.post(url, headers=headers, json=payload)
+        response_data = response.json()
+
+        # Check if the response contains an error
+        if "error" in response_data:
+            return JsonResponse(response_data, status=response.status_code)
+
+        return JsonResponse(response_data)
+    except requests.exceptions.RequestException as e:
+        return JsonResponse({"error": "Error making API request.", "details": str(e)}, status=500)
+    except json.JSONDecodeError:
+        return JsonResponse({"error": "Error parsing API response."}, status=500)
